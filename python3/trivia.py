6d1eac72554eab3b44dad9013fab913d418409dd
Kevin Burleigh <klb@kindling>
2019-10-17 16:30:59 -0500

    fix indentation/whitespace

--- python3/trivia.py
+++ python3/trivia.py
@@ -2,15 +2,15 @@
 
 class Game:
     def __init__(self):
-        self.players = []
-        self.places = [0] * 6
-        self.purses = [0] * 6
+        self.players        = []
+        self.places         = [0] * 6
+        self.purses         = [0] * 6
         self.in_penalty_box = [0] * 6
 
-        self.pop_questions = []
+        self.pop_questions     = []
         self.science_questions = []
-        self.sports_questions = []
-        self.rock_questions = []
+        self.sports_questions  = []
+        self.rock_questions    = []
 
         self.current_player = 0
         self.is_getting_out_of_penalty_box = False
@@ -56,8 +56,8 @@ class Game:
                     self.places[self.current_player] = self.places[self.current_player] - 12
 
                 print(self.players[self.current_player] + \
-                            '\'s new location is ' + \
-                            str(self.places[self.current_player]))
+                      '\'s new location is ' + \
+                      str(self.places[self.current_player]))
                 print("The category is %s" % self._current_category)
                 self._ask_question()
             else:
@@ -69,27 +69,27 @@ class Game:
                 self.places[self.current_player] = self.places[self.current_player] - 12
 
             print(self.players[self.current_player] + \
-                        '\'s new location is ' + \
-                        str(self.places[self.current_player]))
+                  '\'s new location is ' + \
+                  str(self.places[self.current_player]))
             print("The category is %s" % self._current_category)
             self._ask_question()
 
     def _ask_question(self):
-        if self._current_category == 'Pop': print(self.pop_questions.pop(0))
+        if self._current_category == 'Pop':     print(self.pop_questions.pop(0))
         if self._current_category == 'Science': print(self.science_questions.pop(0))
-        if self._current_category == 'Sports': print(self.sports_questions.pop(0))
-        if self._current_category == 'Rock': print(self.rock_questions.pop(0))
+        if self._current_category == 'Sports':  print(self.sports_questions.pop(0))
+        if self._current_category == 'Rock':    print(self.rock_questions.pop(0))
 
     @property
     def _current_category(self):
-        if self.places[self.current_player] == 0: return 'Pop'
-        if self.places[self.current_player] == 4: return 'Pop'
-        if self.places[self.current_player] == 8: return 'Pop'
-        if self.places[self.current_player] == 1: return 'Science'
-        if self.places[self.current_player] == 5: return 'Science'
-        if self.places[self.current_player] == 9: return 'Science'
-        if self.places[self.current_player] == 2: return 'Sports'
-        if self.places[self.current_player] == 6: return 'Sports'
+        if self.places[self.current_player] ==  0: return 'Pop'
+        if self.places[self.current_player] ==  4: return 'Pop'
+        if self.places[self.current_player] ==  8: return 'Pop'
+        if self.places[self.current_player] ==  1: return 'Science'
+        if self.places[self.current_player] ==  5: return 'Science'
+        if self.places[self.current_player] ==  9: return 'Science'
+        if self.places[self.current_player] ==  2: return 'Sports'
+        if self.places[self.current_player] ==  6: return 'Sports'
         if self.places[self.current_player] == 10: return 'Sports'
         return 'Rock'
 
@@ -99,30 +99,25 @@ class Game:
                 print('Answer was correct!!!!')
                 self.purses[self.current_player] += 1
                 print(self.players[self.current_player] + \
-                    ' now has ' + \
-                    str(self.purses[self.current_player]) + \
-                    ' Gold Coins.')
+                      ' now has ' + \
+                      str(self.purses[self.current_player]) + \
+                      ' Gold Coins.')
 
                 winner = self._did_player_win()
                 self.current_player += 1
                 if self.current_player == len(self.players): self.current_player = 0
-
                 return winner
             else:
                 self.current_player += 1
                 if self.current_player == len(self.players): self.current_player = 0
                 return True
-
-
-
         else:
-
             print("Answer was corrent!!!!")
             self.purses[self.current_player] += 1
             print(self.players[self.current_player] + \
-                ' now has ' + \
-                str(self.purses[self.current_player]) + \
-                ' Gold Coins.')
+                 ' now has ' + \
+                 str(self.purses[self.current_player]) + \
+                 ' Gold Coins.')
 
             winner = self._did_player_win()
             self.current_player += 1
@@ -141,7 +136,6 @@ class Game:
 
     def _did_player_win(self):
         return not (self.purses[self.current_player] == 6)
-
 
 from random import randrange
 
